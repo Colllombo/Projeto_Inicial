3605f1d83e4c8a8d7c1edf30d404d4f8e7b5fddd
Jornada de Dados <gabrielvenic@gmail.com>
2025-11-10 17:37:00 -0300

    Finalizado exercício 2

--- Aulas/aula02/Exercícios.py
+++ Aulas/aula02/Exercícios.py
@@ -1,12 +1,17 @@
-# #### Inteiros (`int`)
+# #### INTEIROS (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero_01 = int(input("Insira o primeiro número: "))
-numero_02 = int(input("Insira o segundo número: "))
-resultado = numero_01 + numero_02
-print(f"O seu resultado é de: {resultado}")
+#numero_01 = int(input("Insira o primeiro número: "))
+#numero_02 = int(input("Insira o segundo número: "))
+#resultado = numero_01 + numero_02
+#print(f"O seu resultado é de: {resultado}")
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+numerador = float(input("Digite o seu numerador: "))
+denominador = float(input("Digite o seu denominador: "))
+resto = int(numerador % denominador)
+print(f"O resto de {int(numerador)} / {int(denominador)} é {resto}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #numero_01 = int(input("Inserir um numero inteiro: "))
@@ -16,7 +21,7 @@ print(f"O seu resultado é de: {resultado}")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
-# #### Números de Ponto Flutuante (`float`)
+# #### NÚMEROS DE PONTO FLUTUANTE (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
@@ -28,7 +33,7 @@ print(f"O seu resultado é de: {resultado}")
 #area_do_circulo = math.pi * raio_do_circulo ** 2
 #print(f"{area_do_circulo}")
 
-# #### Strings (`str`)
+# #### STRINGS (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
@@ -41,7 +46,7 @@ print(f"O seu resultado é de: {resultado}")
 #print(f"O elemento 2 e 0: {lista_de_dia_mes_ano[1]}")
 #print(f"O elemento 3 e 0: {lista_de_dia_mes_ano[2]}")
 
-# #### Booleanos (`bool`)
+# #### BOOLEANOS (`bool`)
 
 # 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
 # 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
@@ -49,7 +54,7 @@ print(f"O seu resultado é de: {resultado}")
 # 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
 # 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
 
-# #### try-except e if
+# #### TRY-EXCEPT E IF
 
 # 21: Conversor de Temperatura
 # 22: Verificador de Palíndromo
