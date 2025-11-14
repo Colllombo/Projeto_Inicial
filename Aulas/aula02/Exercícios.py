8aa12fc0042405c4c5048e8727ba6955764e0f47
Jornada de Dados <gabrielvenic@gmail.com>
2025-11-14 08:55:06 -0300

    Exercício 12 finalizado!

--- Aulas/aula02/Exercícios.py
+++ Aulas/aula02/Exercícios.py
@@ -63,11 +63,15 @@
 # #### STRINGS (`str`)
 
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-nome = input("Digite seu nome: ").upper()
+#nome = input("Digite seu nome: ").upper()
+## nome_maiusculo = nome.upper() segunda opção
+#print(f"Seu nome é: {nome}")
+
+# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+nome = input("Digite seu nome: ").lower()
 ## nome_maiusculo = nome.upper() segunda opção
 print(f"Seu nome é: {nome}")
 
-# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
